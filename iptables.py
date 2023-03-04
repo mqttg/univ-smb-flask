47eacfc7ca09574ac93416bec51fe06fb21c6cf8
Matteo Gamba <matt.gamba.chabert@gmail.com>
2023-03-04 13:56:26 +0000

    Flask

---
+++ iptables.py
@@ -0,0 +1,30 @@
+# save this as app.py
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/start")
+def index():
+    return render_template("index.html")
+
+@app.route("/rules_filter")
+def rules_filter():
+    return render_template("rules_filter.html")
+
+@app.route("/rules_nat")
+def rules_nat():
+    return render_template("ules_nat.html")
+
+@app.route("/rules_nat_add")
+def rules_nat_add():
+    return "?"
+
+@app.route("/alias")
+def alias():
+    return render_template("alias.html")
+
